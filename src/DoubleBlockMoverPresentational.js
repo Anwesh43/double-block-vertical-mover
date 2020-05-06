94b8b613f42ea410779968d6707d0b3b219695dc
Anwesh43 <anweshthecool0@gmail.com>
2020-05-06 07:34:49 +0530

    adding DoubleBlockMoverPresentational component

---
+++ src/DoubleBlockMoverPresentational.js
@@ -0,0 +1,23 @@
+import {useStyle} from './hooks'
+import React from 'react'
+
+const VerticalBlock = ({w, h, scale}) => {
+    const {tStyle} = useStyle(scale, w, h)
+    return <div style = {tStyle()}>
+    </div>
+}
+
+const HorizontalBlock = ({i, w, h, scale}) => {
+    const {blockStyle} = useStyle(scale, w, h)
+    return <div onClick = {onClick} style = {blockStyle(i)}>
+    </div>
+}
+
+const DoubleBlockMoverPresentational = ({w, h, scale, onClick}) => {
+    return <div onClick = {onClick}>
+              {[0, 1].map(i => <HorizontalBlock key = {`hb_${i}`} w = {w} h = {h} scale = {scale}/>)}
+              <VerticalBlock w = {w} h = {h} scale = {scale}/>
+          </div>
+}
+
+export default DoubleBlockMoverPresentational
